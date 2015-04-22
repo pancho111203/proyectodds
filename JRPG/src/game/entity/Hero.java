1d5b42ca2c7bd02ea9c2c24ef9fb3022e0a89211
marsafe1 <marsafe1@inf.upv.es>
2015-04-22 17:33:27 +0200

    test2

---
+++ JRPG/src/game/entity/Hero.java
@@ -0,0 +1,33 @@
+package game.entity;
+
+import java.awt.event.KeyEvent;
+import java.util.Random;
+
+import game.graphics.RenderingLevel;
+
+public class Hero extends Entity {
+	
+	private final int speed = 2;
+	
+	
+	public Hero(int x, int y) {
+		super(x, y);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update() {
+		
+
+	}
+
+	@Override
+	public void render(RenderingLevel render) {
+
+	}
+	
+	public void animate(){
+		
+	}
+
+}
