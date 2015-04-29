4b923299199445148b1bc7c528bf40e248ba45f5
marsafe1 <marsafe1@inf.upv.es>
2015-04-29 18:50:52 +0200

    added renderImage() & class Image

--- JRPG/src/game/graphics/RenderingMainMenu.java
+++ JRPG/src/game/graphics/RenderingMainMenu.java
@@ -5,5 +5,24 @@ public class RenderingMainMenu extends Rendering{
 	public RenderingMainMenu(int width, int height) {
 		super(width, height);
 	}
+	
+	public void renderImage(int xp, int yp, Image sp){ // (xp,yp) es la posicion del sprite respecto a la pantalla(en pixels!!)
+		
+		int spriteHeight = sp.getHeight();
+		int spriteWidth = sp.getWidth();
+		
+		for(int y=0;y<spriteHeight;y++){// (x,y) es la posicion del pixel respecto al sprite
+			int ya = (yp+y); // (xa,ya) posicion del pixel al que hay que pintar en la pantalla
+			for(int x=0;x<spriteWidth;x++){
+				int xa = (xp+x);
+				
+				
+				if(xa >= width || xa<0 || ya >= height || ya<0){
+					continue;
+				}
 
+				pixels[xa+ya*width]= sp.pixels[x+y*spriteWidth];
+			}
+		}
+	}
 }
--- JRPG/src/game/states/MainMenuState.java
+++ JRPG/src/game/states/MainMenuState.java
@@ -1,23 +1,33 @@
 package game.states;
 
+import java.io.File;
+
 import game.StateMachine;
+import game.graphics.Image;
 import game.graphics.Rendering;
 import game.graphics.RenderingMainMenu;
+import game.graphics.Spritesheet;
 
 public class MainMenuState implements IState{
 
 	@SuppressWarnings("unused")
 	private StateMachine game;
 	private RenderingMainMenu render;
+	
+	private Image bg;
+	private Image btn;
+	private Image sel;
+	
 	//TODO Main Menu Methods
 	public MainMenuState(StateMachine game, int w, int h){
 		this.game = game;
 		render = new RenderingMainMenu(w,h);
+		bg = new Image("/images/pilar_pequeno.png");
 	}
 	
 	@Override
 	public void render() {
-		
+		render.renderImage(0, 0, bg);
 	}
 
 	@Override
