07542cf615577e3607c3ebaacb50c93d34804c6a
marsafe1 <marsafe1@inf.upv.es>
2015-05-06 02:24:41 +0200

    dfh

--- JRPG/src/game/level/FirstLevel.java
+++ JRPG/src/game/level/FirstLevel.java
@@ -2,6 +2,7 @@ package game.level;
 
 import game.graphics.RenderingLevel;
 import game.graphics.Spritesheet;
+import game.sound.SoundManager;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -12,7 +13,6 @@ import javax.imageio.ImageIO;
 public class FirstLevel extends Level {
 	
 	private String imgLevelPath;
-	private BufferedImage imgToLvL;
 	
 	public FirstLevel(int startPosX, int startPosY, String pathImgToLvL, int w, int h){
 		super(startPosX,startPosY,w,h);
@@ -20,24 +20,15 @@ public class FirstLevel extends Level {
 		loadLevel();
 	}
 	
-	public FirstLevel(int startPosX, int startPosY, BufferedImage imgToLvL, int w, int h){
-		super(startPosX,startPosY,w,h);
-		imgLevelPath = null;
-		this.imgToLvL=imgToLvL;
-		loadLevel();
-	}
-	
 	@Override
 	public void loadLevel(){
-		loadLevelFromImage();
+		loadLevelFromImage(imgLevelPath);
 	}
 	
 	
-	public void loadLevelFromImage(){
+	public void loadLevelFromImage(String path){
 		try{
-			BufferedImage image;
-			if(imgLevelPath==null) image = imgToLvL;
-			else image = ImageIO.read(Level.class.getResource(this.imgLevelPath));
+			BufferedImage image = ImageIO.read(Level.class.getResource(path));
 			width=image.getWidth();
 			height=image.getHeight();			
 			tiles=new int[width*height];
@@ -54,37 +45,51 @@ public class FirstLevel extends Level {
 	public void render(RenderingLevel render){
 		super.render(render);
 		
+		SoundManager.getSingleton("FirstLevel").play(1); 
 	}
 
 	@Override
 	public void initializeSpritesAndTiles() {
 		//TODO herramienta para facilitar creacion de sprites y tiles
-		int hexValuesCol[] = {0xff00f6ff,0xff00f6f1,0xff00f6f2,0xff00f6f3,
-				0xff00f6f4,0xff00f6f5,0xff00f6f6,0xff00f6f7,0xff00f6f8,
-				0xff00f6f9,0xff00f6fa,0xff00f6fb,0xff00f6fc,0xff00f6fd,
-				0xff00f6fe,0xff00f700,0xff00f701,0xff00f702,0xff00f703,
-				0xff00f704,0xff00f705,0xff00f706,0xff00f707,0xff00f708
-		};
 		
-		int i = 0;
+		//pilar
+		int pilar = 0;
+		for(int y = 0;y<3;y++){
+			for(int x=0;x<2;x++){
+				spr_t.initSpriteOnTileOnHex("borde"+x+y, x, y, Spritesheet.test, 0xff00f6f0+pilar);
+				pilar++;
+			}
+		}
+		//suelo
+		spr_t.initSpriteOnTileOnHex("suelo", 1, 4, Spritesheet.test, 0xffff7f27); //y: 0 y 1 diferentes suelos
+		//borde grande
+		spr_t.initSpriteOnTileOnHex("bordeGra", 1, 3, Spritesheet.test, 0xfffd0691);
+		//borde lateral derecha
+		spr_t.initSpriteOnTileOnHex("bordeDer", 2, 2, Spritesheet.test, 0xfffff200);
+		//borde lateral izquierda
+		spr_t.initSpriteOnTileOnHex("bordeIzq", 3, 2, Spritesheet.test, 0xfffd0634);
+		//cielo
+		spr_t.initSpriteOnTileOnHex("cielo", 0, 3, Spritesheet.test, 0xffa349a4);
+		//puerta
+		int puerta = 0;
 		for(int y = 0;y<6;y++){
 			for(int x=0;x<8;x++){
- 				spr_t.initSpriteOnTileOnHex("puerta"+x+y, 4+x, y, Spritesheet.test, 0xff28a661+puerta);
- 				puerta++;
- 			}
- 		}
- 		//paret
- 		int paret = 0;
- 		for(int y = 0;y<2;y++){
- 			for(int x=0;x<2;x++){
- 				spr_t.initSpriteOnTileOnHex("paret"+x+y, 2+x, y, Spritesheet.test, 0xff06599c+paret);
- 				paret++;
- 			}
- 		}
- 		
- 		
+				spr_t.initSpriteOnTileOnHex("puerta"+x+y, 4+x, y, Spritesheet.test, 0xff28a661+puerta);
+				puerta++;
+			}
+		}
+		//paret
+		int paret = 0;
+		for(int y = 0;y<2;y++){
+			for(int x=0;x<2;x++){
+				spr_t.initSpriteOnTileOnHex("paret"+x+y, 2+x, y, Spritesheet.test, 0xff06599c+paret);
+				paret++;
+			}
+		}
+		
+		
+	
 	}
 
-		
 
 }
