fd743e8cbb1aa0844a622195b86db8f4d516c44f
pancho111203 <pancho_111203@hotmail.com>
2015-06-04 20:53:46 +0200

    pequeños cambios

--- JRPG/src/game/entity/modules/EnergyModule.java
+++ JRPG/src/game/entity/modules/EnergyModule.java
@@ -2,9 +2,16 @@ package game.entity.modules;
 
 public class EnergyModule {
 	private int energy, maxEnergy;
+	
+	private int emptiedLockQuant;
+	
+	private boolean emptied = false;
+	
 	public EnergyModule(int startingEnergy, int maxEnergy){
 		energy = startingEnergy;
 		this.maxEnergy = maxEnergy;
+		
+		emptiedLockQuant = maxEnergy/10;
 	}
 	public void setEnergy(int e){
 		if(e<=maxEnergy)energy=e;
@@ -18,7 +25,16 @@ public class EnergyModule {
 	
 	//devuelve -1 si es invalido
 	public int substractEnergy(int x){
+		if(emptied&&energy<emptiedLockQuant){
+			return -1;
+		}
+			
+		if(emptied){
+			emptied = false;
+		}
+		
 		if(energy-x<=0){
+			emptied = true;
 			return -1;
 		}
 		energy-=x;
