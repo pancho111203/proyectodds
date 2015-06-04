083e25e7cfee0a2158c270e535021ad05a5c5df6
pancho111203 <pancho_111203@hotmail.com>
2015-06-04 19:04:43 +0200

    añadido dmgmodule

---
+++ JRPG/src/game/entity/modules/DMGModule.java
@@ -0,0 +1,17 @@
+package game.entity.modules;
+
+public class DMGModule {
+	private int DMG;
+	
+	public DMGModule(int dmg){
+		DMG = dmg;
+	}
+	
+	public int getDMG() {
+		return DMG;
+	}
+
+	public void setDMG(int dMG) {
+		DMG = dMG;
+	}
+}
